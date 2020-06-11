9830b69fcc15413570c491b03e768788f35ede4d
madoska <medina-dadurgova@hotmail.com>
2020-06-11 18:00:02 +0200

    html + css done

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
               <li><a href="#contact">Contact</a></li>
 
               <div class="dash">
-                <a href="LINK SALESFORCE">Dashboard</a>
+                <a href="https://login.salesforce.com/?locale=nl">Dashboard</a>
               </div>
             </ul>
           </div>
@@ -50,7 +50,7 @@
       </div>
     </div>
 
-    <div class="video-container">
+    <div class="video-container" id="tuur">
       <div class="video">
         <video width="640" height="360" controls autoplay muted>
           <source src="/images/Tuur_promo.mp4" type="video/mp4" />
@@ -91,27 +91,28 @@
       </div>
     </div>
 
-    <div class="product-box">
-      <div class="product1">
-        <img class="box-img" src="https://via.placeholder.com/150x150" alt="" />
-        <p class="box-txt">Aai zijn zachte vacht en praat tegen Tuur.</p>
+    <div class="product-box" id="product">
+      <div class="product">
+        <img class="box-img" src="/images/tuur-fur.png" alt="" />
+        <p class="box-txt">Aai zijn zachte vacht <br>en praat tegen Tuur.</p>
       </div>
-      <div class="product2">
-        <img src="https://via.placeholder.com/150x150" alt="" class="box-img" />
-        <p class="box-txt">Zijn staart beweegt wanneer je hem streelt.</p>
+      <div class="product">
+        <img src="/images/tuur-tail.png" alt="" class="box-img" />
+        <p class="box-txt">Zijn staart beweegt <br>wanneer je hem streelt.</p>
       </div>
-      <div class="product3">
-        <img src="https://via.placeholder.com/150x150" alt="" class="box-img" />
-        <p class="box-txt">Tuur meet je vitale data en visualiseert ze.</p>
+      <div class="product">
+        <img src="/images/tuur-connectedpatient.png" alt="" class="box-img" />
+        <p class="box-txt">Tuur meet je vitale data <br>en visualiseert ze.</p>
       </div>
     </div>
 
+  <div class="product-container1">
     <div class="product-img">
-      <img class="img1" src="https://via.placeholder.com/250x123" alt="" />
-      <img src="https://via.placeholder.com/250x123" alt="" class="img2" />
+      <img class="product-tuur" src="images/tuur-body.png" alt="" />
     </div>
 
     <div class="product-txt">
+      <h2 class="product-heading">Een tedere vriend</h2>
       <div class="txt1">
         <p>
           In tegenstelling tot echte husidieren heeft Tuur geen verzorging
@@ -122,7 +123,7 @@
       </div>
       <div class="txt2">
         <p>
-          Tuur is ontwikkelt door IMD-studentes Annelies en Medina als project
+          Tuur is ontwikkelt door IMD-studentes Annelies Deroy en Medina Dadurgova als project
           voor het vak Lab 2. Het idee is ontstaan uit het concept Smart Elderly
           Care. Uit ons research hebben wij geleerd dat eenzaamheid een groot
           probleem is bij ouderen dat vaak onopgemerkt blijft en daarnaast ook
@@ -132,31 +133,126 @@
         </p>
       </div>
     </div>
+  </div>
 
-    <div class="faq">
+  <div class="product-container2">
+    <div class="product-txt2">
+      <h2 class="product-heading">Tuur zorgt voor jou</h2>
+      <div class="txt3">
+        <p>
+          Tuur's hartslagmeter is wat hem zo uniek maakt. Hij heeft geen zorg nodig, maar wil juist zorgen voor jou.
+        </p>
+      </div>
+      <div class="txt4">
+        <p>
+          Wij zijn gepartnerd met Salesforce, een bedrijf gespecialiseerd in onder andere het beheer van medische gegevens. Hierdoor kunnen we een zeer betrouwbaar systeem aanbieden dat verzorgers toestaat de gezondheid van hun ouderen in één oogopslag te monitoren.
+        </p>
+      </div>
+      <div class="txt-extra">
+        <p>
+          Heb je Tuur al?
+        </p>
+        <div class="txt-dash">
+          <a href="https://login.salesforce.com/?locale=nl">Ga naar jouw dashboard</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="product-img2">
+      <img class="product-salesforce" src="images/salesforce.png" alt="" />
+    </div>
+  </div>
+
+
+
+    <div class="faq" id="faq">
       <div class="faq1">
-        <a href="#q1" class="q">+ Waar kan ik Tuur aankopen?</a>
-        <p class="a1">
+        <p class="q">Waar kan ik Tuur aankopen?</p>
+        <p class="a">
           Tuur is een project voor het vak Lab 2 van de Thomas More Hoge School
           bachelor Interactieve Multimedia Design en (nog) niet beschikbaar voor
-          aankoop.
+          aankoop. Om op de hoogte te blijven van eventuele updates raden wij u aan 
+          zich aan te melden voor onze nieuwsbrief. U kan ook gerust contact met ons opnemen 
+          via onderstaand contactformulier.
         </p>
       </div>
       <div class="faq2">
-        <a href="#q2" class="q">+ Waarvoor dient Tuur?</a>
-        <p class="a2">
+        <p class="q">Waarvoor dient Tuur?</p>
+        <p class="a">
           Tuur is een robot huisdier voor ouderen die niet langer in staat zijn
           echte huisdieren te houden. Met Tuur hopen wij eenzaamheid bij ouderen
-          tegen te gaan.
+          tegen te gaan en tegelijkertijd ook hun verzorgers helpen met het monitoren van de gezondheid.
         </p>
       </div>
       <div class="faq3">
-        <a href="#q3" class="q">+ Helpt een robot huisdier echt?</a>
-        <p class="a3">
+        <p class="q">Helpt een robot huisdier echt?</p>
+        <p class="a">
           Studies tonen aan dat robot huisdieren positieve emoties kunnen
-          opwekken bij ouderen.
+          opwekken bij ouderen. Niet alleen worden ze gelukkiger van zijn snoezig zacht uiterlijk, 
+          maar het is bewezen dat ouderen ook socialer worden.
         </p>
       </div>
     </div>
+
+    <div class="cat-pink">
+      <img class="cat-pink-img" src="/images/cat-pink.png" />
+    </div>
+
+    <div class="contact-container">
+      <p class="newsletter-title">Geen antwoord gevonden op uw vraag?</p>
+      <p class="newsletter-sub">
+        Aarzel niet om contact op te nemen met ons.
+      </p>
+      <div class="form">
+        <form action="" method="post" class="contact-form">
+          <input type="text" name="email" id="contact-email" placeholder="email" />
+            <textarea name="contact" id="contact-email" placeholder="Uw vraag hier..." cols="100" rows="10"></textarea>
+          <input type="verzend" id="verzend" value="Verzend" />
+        </form>
+      </div>
+    </div>
+
+    <div class="info">
+      <div class="logo-black">
+        <img src="/images/logo-black.png" alt="" class="logo-info">
+      </div>
+
+      <div class="items">
+        <div class="links-info">
+          <ul class="info-items">
+            <li><a href="#tuur">Over Tuur</a></li>
+    
+            <li><a href="#product">Product</a></li>
+    
+            <li><a href="#faq">FAQ</a></li>
+    
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+    
+        <div class="info-contact">
+          <ul class="contact-items">
+            <li><b class="info-bold">Contact</b></li>
+            <li>015 36 91 00</li>
+    
+            <li><a href="mailto:tuurcare@gmail.com">tuurcare@gmail.com</a></li>
+          </ul>
+        </div>
+    
+        <div class="info-location">
+          <ul class="location-items">
+            <li><b class="info-bold">IMD</b></li>
+            <li>Thomas More Hoge School</li>
+            <li>Zandpoortvest 60</li>
+            <li>2800 Mechelen</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <img src="/images/line-horizontal.png" alt="" class="footer">
+      <p class="cr">© Annelies Deroy, Medina Dadurgova — 2020</p>
+    </footer>
   </body>
 </html>
